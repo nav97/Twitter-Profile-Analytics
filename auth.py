56b5e56559ebf969094fefc5d60178d47d2baee6
nav97 <nav97@users.noreply.github.com>
2017-02-23 22:02:46 -0500

    Added auth file

---
+++ auth.py
@@ -0,0 +1,13 @@
+"""
+Create a twitter account and then go to https://apps.twitter.com/
+to create an app to generate a consumer key
+"""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+
+"""
+Thereafter, you will be redirected to your app's page
+where you can create an access token
+"""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
