74fcf005fb3e9bda8139f09f311b53832c1a401e
nav97 <nav97@users.noreply.github.com>
2017-04-11 23:44:52 -0400

    Added basic heatmap functionality and more data tracking

--- tweet_analyzer.py
+++ tweet_analyzer.py
@@ -5,9 +5,10 @@ import collections
 import datetime
 import tweepy
 import numpy as np
-from ascii_graph import Pyasciigraph
-from ascii_graph.colors import Gre, Yel, Red
-from ascii_graph.colordata import hcolor
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 from tqdm import tqdm
 
 try:
@@ -40,10 +41,14 @@ detectedUrls = collections.Counter()
 detectedRetweets = collections.Counter()
 detectedHashtags = collections.Counter()
 mentionedUsers = collections.Counter()
+retweetedUsers = collections.Counter()
 detetctedLocations = collections.Counter()
 detectedLanguages = collections.Counter()
 
 
+hourlyActivity = {"%.2d:00" %hour: 0 for hour in range (24)}
+weeklyActivity = {"%s" %day: 0 for day in range(7)}
+activityMatrix = np.zeros((7, 24))
 
 
 """ Process and analyze a single tweet, updating our data"""
@@ -55,38 +60,64 @@ def process_tweet(tweet):
     dateOfTweet = tweet.created_at
     endDate = endDate or dateOfTweet
     startDate = dateOfTweet
-
     
     #Update time based off timezone and specified offset
 
     #Update hourly activity -> Goes to graph
+    hourlyActivity["%.2d:00" %dateOfTweet.hour] += 1
 
     #Update weekly activity -> Goes to graph
+    weeklyActivity[str(dateOfTweet.weekday())] += 1
+
+    #Update activityMatrix for heatmap
+    activityMatrix[dateOfTweet.weekday()][dateOfTweet.hour] += 1
 
     #Update Domain Urls detected
+    if(tweet.entities['urls']):
+        for url in tweet.entities['urls']:
+            domain = urlparse(url['expanded_url']).netloc
+            detectedUrls[domain] += 1 if (domain != "twitter.com") else (0)
 
     #Update Retweets
+    if hasattr(tweet, 'retweeted_status'):
+        retweetedUsers[tweet.retweeted_status.user.screen_name] += 1
 
     #Update Hashtag List
+    if(tweet.entities['hashtags']):
+        for hashtag in tweet.entities['hashtags']:
+            detectedHashtags[hashtag['text']] += 1
 
     #Update Mentioned Users
+    if(tweet.entities['user_mentions']):
+        for user in tweet.entities['user_mentions']:
+            mentionedUsers[user['screen_name']] += 1
 
     #Update detetcted locations
 
     #Update detected languages
 
+    #Update detected devices
+
 
 
 """ Download Tweets from user account """
 def get_tweets(api, user, limit):
-    for tweet in tqdm(tweepy.Cursor(api.user_timeline, screen_name = user).items(limit), bar_format='l_bar'):
+    for tweet in tqdm(tweepy.Cursor(api.user_timeline, screen_name=user).items(limit), unit="tweet", total=limit):
         process_tweet(tweet)
+    
+    create_graphs()
 
-def print_stats():
+def print_report():
     return
 
-def print_charts():
-    return
+def create_graphs():
+
+    Index = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    Cols = ["%.2d:00" %x for x in range(24)]
+    activity = pd.DataFrame(activityMatrix, index=Index, columns=Cols)
+
+    heatmap = sns.heatmap(activity, annot=True, linewidths=0.5)
+    plt.show()
 
 
 def main():
@@ -123,8 +154,8 @@ if __name__ == "__main__":
     try:
         main()
     except tweepy.error.TweepError as e:
-        print("Twitter error: %s" %e)
+        print("\nTwitter error: %s" %e)
     except Exception as e:
-        print("Error: %s" %e)
+        print("\nError: %s" %e)
         traceback.print_exc()
 
